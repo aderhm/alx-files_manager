9540050df964759e0d352191762efd5b6270c0f0
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-09-04 10:33:28 +0100

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 import { APIError, errorResponse } from '../middlewares/error';
 
 // Injects routes with their handlers to the given Express application.
@@ -9,7 +11,11 @@ const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
 
+  api.get('/connect', basicAuthenticate, AuthController.getConnect);
+  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+
   api.post('/users', UsersController.postNew);
+  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
   api.all('*', (req, res, next) => {
     errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
