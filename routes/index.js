6324d5b681e051bcaff5c5ad4c8a236bb5d78d43
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-09-04 10:24:38 +0100

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 import { APIError, errorResponse } from '../middlewares/error';
 
 // Injects routes with their handlers to the given Express application.
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+
+  api.post('/users', UsersController.postNew);
 
   api.all('*', (req, res, next) => {
     errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
